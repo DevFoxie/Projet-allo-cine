cb54b4e8919bf22ce4f840706a2efb4fb70d6d47
DevFoxie <hmerniz.dev@gmail.com>
2023-01-03 10:16:48 +0100

    final_version

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
 fetch('./data/movies.json')
-    .then(function(response){
-        return response.json()
-    })
-    .then(function(data) {
-        console.log('Response json : ', data)
-        console.log(data[0].Title)
+  .then(function (response) {
+    return response.json()
+  })
+  .then(function (data) {
+    console.log('Response json : ', data)
+    console.log(data[0].Title)
 
     let div = document.querySelector('.movies')
-        data.forEach(movie => {
-            div.innerHTML = div.innerHTML += `<div class="row">
+    data.forEach(movie => {
+      div.innerHTML = div.innerHTML += `<div class="row">
             
             <div class="col s6 film_1"></div>
                 <div class="row">
 
-                <div class="col s12 m7">
+                <div class="col s12 m7 filmcard">
                 <div id="bloc">
                 <h1 class="header">${movie.Title}</h1>
                 </div>
-                <div class="card horizontal">
+                <div class="card horizontal medium">
                   <div class="card-image">
                     <img src="${movie.Poster}">
                   </div>
@@ -39,15 +39,21 @@ fetch('./data/movies.json')
             
                 </div>
                 <div class="row">
-                    <div class="col">
-                    <img src="./Images/imdb.png" alt="imdb" class="imdb">${data[0].Metascore} <img src="./Images/metacritic.png" alt="metacritic" class="metacritic">${data[0].imdbRating}
+                    <div class="col s12 notation">
+                    <img src="./Images/imdb.png" alt="imdb" class="imdb">${movie.Metascore} <img src="./Images/metacritic.png" alt="metacritic" class="metacritic">${movie.imdbRating}
                     </div>
-                    <div class="col">
-                        <img src="${movie.Images}" alt="" class="image_2">
+                    <div class="col s12">
+                    <div class="carousel" id="demo-carousel">
+                    <a class="carousel-item" href="#one!"><img src="${movie.Images[0]}"></a>
+                    <a class="carousel-item" href="#two!"><img src="${movie.Images[1]}"></a>
+                    <a class="carousel-item" href="#three!"><img src="${movie.Images[2]}"></a>
+                    <a class="carousel-item" href="#four!"><img src="${movie.Images[3]}"></a>
+                    <a class="carousel-item" href="#five!"><img src="${movie.Images[4]}"></a>
+                  </div>
                     </div>
                 </div>
             </div>
             <hr>
         </div>`
-        })
-})
+    })
+  })
